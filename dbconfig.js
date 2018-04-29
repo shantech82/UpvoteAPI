6b09e77f7c5fb76062e3d7014cf61f47dc4099be
Shanmuga <Sundaram.Shanmuga@cognizantog.com>
2018-04-29 12:33:10 +0200

    database config change

--- dbconfig.js
+++ dbconfig.js
@@ -7,13 +7,13 @@ var options = {
 var pgp = require('pg-promise')(options);
 
 var connectionstring = {
-    host: 'localhost', // server name or IP address;
+    host: 'ec2-54-246-84-200.eu-west-1.compute.amazonaws.com', // server name or IP address;
     port: 5432,
-    database: 'upvote',
-    user: 'upvoteuser',
-    password: 'upvoteuser'
+    database: 'd9setko6ee7es3',
+    user: 'yqncyvhhhxolom',
+    password: 'a0780cb77d6e29d0eece2a66dbba193aef6b74cd555f2c230cde5868ba18fe43',
+    ssl: true
 };
-
 var db = pgp(connectionstring);
 
 module.exports =  db;
